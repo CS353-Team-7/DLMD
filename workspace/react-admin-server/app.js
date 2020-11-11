d6761ed7eef16be45f9a6a7087512b0d14d1efa3
GUOWEN LIU <liugw01@outlook.com>
2020-11-11 02:08:19 +0000

    Add Nodejs files for the server to manipulate the database

--- workspace/react-admin-server/app.js
+++ workspace/react-admin-server/app.js
@@ -4,7 +4,13 @@ app.use(express.json());
 app.listen(5001,()=>{
     console.log('running server')
 });
+//MongoDB
+
+
+
+
 //load router
 const router = require("./router");
 
 app.use(router);
+app.use(express.static('public'));
