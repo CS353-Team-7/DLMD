a264d735eb587b1e1bbe59a6be1e7043c6611863
CameronC45 <73218522+CameronC45@users.noreply.github.com>
2020-11-11 10:20:04 +0000

    Add files via upload

---
+++ firebase-auth/scripts/auth.js
@@ -0,0 +1,48 @@
+auth.onAuthStateChanged(user => {
+    if(user) {
+        setupUI(user);
+    }else{
+        setupUI();
+    }
+});
+
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    // sign up the user
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    });
+});
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut();
+});
+
+//log in
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    //get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        
+        // close the login modal and reset thr form
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    })
+})
