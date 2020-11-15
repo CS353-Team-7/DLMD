f3f4fa44efb8a1400329c42cd48f22f5c243b1a9
GUOWEN LIU <liugw01@outlook.com>
2020-11-15 20:15:04 +0000

    Add registration rules and register function

--- workspace/react-admin-client/src/pages/login/r.js
+++ workspace/react-admin-client/src/pages/login/r.js
@@ -1,143 +1,131 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import fire from "../../api/commonFirebase";
 import "antd/dist/antd.css";
-import { Form, Input, Tooltip, Button } from "antd";
+import {Form, Input, Tooltip, Button, message} from "antd";
 import { QuestionCircleOutlined } from "@ant-design/icons";
+import React,{Component,useState} from "react";
 
-const formItemLayout = {
-    labelCol: {
-        xs: {
-            span: 24
-        },
-        sm: {
-            span: 8
-        }
-    },
-    wrapperCol: {
-        xs: {
-            span: 24
-        },
-        sm: {
-            span: 16
-        }
-    }
-};
-const tailFormItemLayout = {
-    wrapperCol: {
-        xs: {
-            span: 24,
-            offset: 0
-        },
-        sm: {
-            span: 16,
-            offset: 8
-        }
-    }
-};
 
-const  RegistrationForm = () => {
-    const [form] = Form.useForm();
-
-    const onFinish = (values) => {
-        console.log("Received values of form: ", values);
+class  RegistrationForm extends Component{
+     formItemLayout = {
+        labelCol: {
+            xs: {
+                span: 24
+            },
+            sm: {
+                span: 8
+            }
+        },
+        wrapperCol: {
+            xs: {
+                span: 24
+            },
+            sm: {
+                span: 16
+            }
+        }
     };
-
-
-    return (
-        <Form
-            {...formItemLayout}
-    form={form}
-    name="register"
-    onFinish={onFinish}
-
-    scrollToFirstError
-    >
-    <Form.Item
-        name="nickname"
-    label={
-        <span>
-        UserName&nbsp;
-    <Tooltip title="This is an important proof of login！ ">
-        <QuestionCircleOutlined />
-        </Tooltip>
-        </span>
-}
-    rules={[
-                {
-                    required: true,
-                    message: "Please input your nickname!",
-                    whitespace: true
-                }
-                ]}
-        >
-        <Input />
-        </Form.Item>
-
-        <Form.Item
-    name="email"
-    label="E-mail"
-    rules={[
-            {
-                type: "email",
-                message: "The input is not valid E-mail!"
+     tailFormItemLayout = {
+        wrapperCol: {
+            xs: {
+                span: 24,
+                offset: 0
             },
-    {
-        required: true,
-            message: "Please input your E-mail!"
-    }
-]}
->
-    <Input />
-    </Form.Item>
-
-    <Form.Item
-    name="password"
-    label="Password"
-    rules={[
-            {
-                required: true,
-                message: "Please input your password!"
+            sm: {
+                span: 16,
+                offset: 8
             }
-            ]}
-    hasFeedback
-    >
-    <Input.Password />
-    </Form.Item>
-
-    <Form.Item
-    name="confirm"
-    label="Confirm Password"
-    dependencies={["password"]}
-    hasFeedback
-    rules={[
-            {
-                required: true,
-                message: "Please confirm your password!"
-            },
-    ({ getFieldValue }) => ({
-        validator(rule, value) {
-            if (!value || getFieldValue("password") === value) {
-                return Promise.resolve();
-            }
-
-            return Promise.reject(
-                "The two passwords that you entered do not match!"
-            );
         }
-    })
-]}
->
-    <Input.Password />
-    </Form.Item>
+    };
+     onFinish = (v) => {
 
-            <Form.Item {...tailFormItemLayout}>
-                <Button type="primary" htmlType="submit">
-                    Register
-                </Button>
-            </Form.Item>
+        fire.auth().createUserWithEmailAndPassword(v.email,v.password).then((u)=>{
+            message.success(v.email+" Create success!")
+        }).catch((error)=>{
+            message.error(error.message);
+        });
+    };
+    render() {
+        return(
+            <Form
+                {...this.formItemLayout}
+                form={this.form}
+                name="register"
+                onFinish={this.onFinish}
 
-        </Form>
+                scrollToFirstError
+            >
+                <Form.Item
+                    name="email"
+                    label="E-mail"
+                    rules={[
+                        {
+                            type: "email",
+                            message: "The input is not valid E-mail!"
+                        },
+                        {
+                            required: true,
+                            message: "Please input your E-mail!"
+                        }
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
 
-);
-};
+                <Form.Item
+                    name="password"
+                    label="Password"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input your password!"
+                        },{
+                            min: 6,
+                            message: "Please enter a length of at least 6 digits"
+                        },
+                    ]}
+                    hasFeedback
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item
+                    name="confirm"
+                    label="Confirm Password"
+                    dependencies={["password"]}
+                    hasFeedback
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please confirm your password!"
+                        },
+                        {
+                            min: 6,
+                            message: "Please enter a length of at least 6 digits"
+                        },
+                        ({ getFieldValue }) => ({
+                            validator(rule, value) {
+                                if (!value || getFieldValue("password") === value) {
+                                    return Promise.resolve();
+                                }
+
+                                return Promise.reject(
+                                    "The two passwords that you entered do not match!"
+                                );
+                            }
+                        })
+                    ]}
+                >
+                    <Input.Password/>
+                </Form.Item>
+
+                <Form.Item {...this.tailFormItemLayout}>
+                    <Button type="primary" htmlType="submit">
+                        Register
+                    </Button>
+                </Form.Item>
+
+            </Form>
+        )
+    }
+}
 export default RegistrationForm;
