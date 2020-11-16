7db5fa8efec5d35c4aed7101acba550a19aa9398
GUOWEN LIU <liugw01@outlook.com>
2020-11-16 23:42:38 +0000

    Add files about some test page

---
+++ workspace/react-admin-client/src/pages/plantcalendar/plantcalendar.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+/*
+
+                <ScheduleComponent>
+                    <Inject services={[Day,Week,WorkWeek,Month,Agenda]}/>
+                </ScheduleComponent>
+* */
+
+
+import {Inject,ScheduleComponent,Day,Week,WorkWeek,Month,Agenda} from '@syncfusion/ej2-react-schedule'
+export default class Home extends Component{
+    render() {
+        return (
+            <div>
+                aaa
+            </div>
+        )
+    }
+}
---
+++ workspace/react-admin-client/src/pages/plantcardsmainview/plantcardsmainview.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+
+
+export default class MainView extends Component{
+    render() {
+        return (
+            <div>
+                This is MainView page
+            </div>
+        )
+    }
+}
