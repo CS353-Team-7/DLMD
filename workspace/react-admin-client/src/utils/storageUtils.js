44897afe57e91885dbcbb0f7ef6dbb3e0860d17e
GUOWEN LIU <liugw01@outlook.com>
2020-11-16 00:02:01 +0000

    Encapsulates the local validation automatic storage function function

---
+++ workspace/react-admin-client/src/utils/storageUtils.js
@@ -0,0 +1,18 @@
+/*
+local data  //"proxy": "http://localhost:5000"
+
+* */
+import store from 'store'
+const USER_KEY = 'user_key'
+export default {
+    saveUser(user){
+        store.set(USER_KEY,user)
+    },
+    getUser(){
+        return store.get(USER_KEY)||{}
+    },
+    removeUser()
+    {
+        store.remove(USER_KEY)
+    }
+}
