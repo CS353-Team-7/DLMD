135b8a04f0fdb32a21353e6fc1051cc3c1abee84
GUOWEN LIU <liugw01@outlook.com>
2020-11-17 13:25:06 +0000

    Modify  calendar page

--- workspace/react-admin-client/src/pages/plantcalendar/plantcalendar.js
+++ workspace/react-admin-client/src/pages/plantcalendar/plantcalendar.js
@@ -1,18 +1,10 @@
 import React, {Component} from 'react';
-/*
 
-* */
-
-
-import {Inject,ScheduleComponent,Day,Week,WorkWeek,Month,Agenda} from '@syncfusion/ej2-react-schedule'
 export default class PlantCalendar extends Component{
     render() {
         return (
             <div>
-
-                <ScheduleComponent>
-                    <Inject services={[Day,Week,WorkWeek,Month,Agenda]}/>
-                </ScheduleComponent>
+                This is plantcalendar page
             </div>
         )
     }
