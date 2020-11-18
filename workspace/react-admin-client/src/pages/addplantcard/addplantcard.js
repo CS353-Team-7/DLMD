a2992e110607256032777dba26ee6be9cebeba3e
GUOWEN LIU <liugw01@outlook.com>
2020-11-18 00:25:00 +0000

    Modification addplantcard file

--- workspace/react-admin-client/src/pages/addplantcard/addplantcard.js
+++ workspace/react-admin-client/src/pages/addplantcard/addplantcard.js
@@ -1,13 +1,45 @@
 import React, {Component} from 'react';
+import { Card ,Rate ,Divider,Tag,Input} from 'antd';
 
+import {
+    EnvironmentTwoTone,FileAddOutlined
+} from '@ant-design/icons';
+import Avatar from './uploadimg'
+import './addplantcard.css'
+
+const { Meta } = Card;
+const desc = ['terrible', 'bad', 'aaa', 'good', 'wonderful'];
 
 
 export default class AddPlantCard extends Component{
+    state = {
+        value: 3,
+    };
+
+    handleChange = value => {
+        this.setState({ value });
+    };
     render() {
+        const { value } = this.state;
+
         return (
-            <div>
-                This is AddPlantCard page
-            </div>
+
+            <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={<Avatar/>}
+            >
+                <Input placeholder="Plant name" />
+
+                <Divider />
+                <Input size="large" placeholder="large size" prefix={<EnvironmentTwoTone />} />
+                <span>
+                    <Rate tooltips={desc} onChange={this.handleChange} value={value} />
+
+                </span>
+            </Card>
         )
     }
 }
+
+
