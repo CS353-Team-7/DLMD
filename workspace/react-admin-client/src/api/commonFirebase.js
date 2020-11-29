0a732207c7da223adb42557139ff25770e6ca021
GUOWEN LIU <liugw01@outlook.com>
2020-11-29 21:55:26 +0000

    Add uuidPack

--- workspace/react-admin-client/src/api/commonFirebase.js
+++ workspace/react-admin-client/src/api/commonFirebase.js
@@ -4,6 +4,8 @@ var firebaseConfig = {
     authDomain: "don-t-let-me-die.firebaseapp.com",
     databaseURL: "https://don-t-let-me-die.firebaseio.com",
     projectId: "don-t-let-me-die",
+    storageBucket: "don-t-let-me-die.appspot.com",
+    messagingSenderId: "430686717248",
     appId: "1:430686717248:web:1a5ff798a609ef67e3e6a5",
     measurementId: "G-DG9PJVNRRK"
 };
--- workspace/react-admin-client/src/utils/memoryUtils.js
+++ workspace/react-admin-client/src/utils/memoryUtils.js
@@ -3,4 +3,5 @@ Some data modules in memory
 * */
 export default {
     user:{},//Save the username who is currently logged in
+    imgUrl:"",
 }
