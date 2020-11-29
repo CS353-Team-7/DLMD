91023c8fb3285fd22c9450137c6c34b5c60b959e
GUOWEN LIU <liugw01@outlook.com>
2020-11-29 21:59:40 +0000

    Modify the plant card content

--- workspace/react-admin-client/src/pages/addplantcard/addplantcard.js
+++ workspace/react-admin-client/src/pages/addplantcard/addplantcard.js
@@ -1,42 +1,124 @@
 import React, {Component} from 'react';
-import { Card ,Rate ,Divider,Tag,Input} from 'antd';
+import { Form, Input, InputNumber, Button,Card } from 'antd';
+import PicturesWall from './uploadimg'
+import memoryUtils from "../../utils/memoryUtils";
+const {Item} = Form
 
-import {
-    EnvironmentTwoTone,FileAddOutlined
-} from '@ant-design/icons';
-import Avatar from './uploadimg'
-import './addplantcard.css'
-
-const { Meta } = Card;
-const desc = ['terrible', 'bad', 'aaa', 'good', 'wonderful'];
 
 
 export default class AddPlantCard extends Component{
-    state = {
-        value: 3,
-    };
+    constructor(props) {
+        super(props);
+        this.pw = React.createRef()
 
-    handleChange = value => {
-        this.setState({ value });
-    };
+    }
+
+    postUserData(values)
+    {
+
+        console.log(values)
+        //Assuming userID is already added?
+        var userURL = "https://don-t-let-me-die.firebaseio.com/plantcard.json/" + memoryUtils.user.username;
+
+
+        fetch("https://don-t-let-me-die.firebaseio.com/plantcard.json", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                //Could also just set an ID as an array in the DB, add the plants to each one (more efficient)
+                ID: memoryUtils.user.username, // REPLACE "test" WITH userID
+                plantName: values.plantname,
+                Note:values.note,
+                Location:values.location,
+                url:values.url
+            })
+        })
+    }
     render() {
-        const { value } = this.state;
+        const layout = {
+            labelCol: {
+                span:2,
+            },
+            wrapperCol: {
+                span: 5,
+            },
+        };
+        const validateMessages = {
+            required: '${label} is required!',
+            types: {
+                email: '${label} is not a valid email!',
+                number: '${label} is not a valid number!',
+            },
+            number: {
+                range: '${label} must be between ${min} and ${max}',
+            },
+        };
+        const onFinish = (values) => {
 
+            values.url = this.pw.current.getImgUrl()
+            this.postUserData(values)
+
+            console.log(values);
+        };
         return (
+            <Card >
+                <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
+                    <Form.Item
+                        name={['plantname']}
+                        label="Plant name"
+                        rules={[
+                            {
+                                required: true,
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
 
-            <Card
-                hoverable
-                style={{ width: 240 }}
-                cover={<Avatar/>}
-            >
-                <Input placeholder="Plant name" />
+                    <Form.Item
+                        name={['note']}
+                        label="Note"
+                        rules={[
+                            {
+                                required: true,
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
 
-                <Divider />
-                <Input size="large" placeholder="large size" prefix={<EnvironmentTwoTone />} />
-                <span>
-                    <Rate tooltips={desc} onChange={this.handleChange} value={value} />
+                    <Form.Item
+                        name={['location']}
+                        label="Location"
+                        rules={[
+                            {
+                                required: true,
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
 
-                </span>
+
+
+                    <Form.Item
+                        name={['picture']}
+                       label="Picture:"
+
+                    >
+                        <PicturesWall ref={this.pw}/>
+                    </Form.Item>
+
+
+                    <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 2 }}>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
             </Card>
         )
     }
